b17a43913339e22f8cdc99455860e446e7d77bb1
chintanpatel <chintanpatel9065@gmail.com>
2025-08-08 18:26:32 +0530

    Add a DepartmentRepository interface with a custom query method.

---
+++ src/main/java/org/chintanpatel/springbootapp/department/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package org.chintanpatel.springbootapp.department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("departmentRepository")
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    List<Department>findByDepartmentNameContainingIgnoreCase(String departmentName);
+}
